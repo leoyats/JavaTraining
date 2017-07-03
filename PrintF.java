624ef72bb36de9d94f86123c74bfcd3f29bff86e
leoyats <leoyats@gmail.com>
2017-07-03 15:00:30 +0300

    Java Training

---
+++ PrintF.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+
+/**
+ * Created by leoyats on 30.06.17.
+ */
+public class PrintF {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c;
+        c = a * b;
+//        System.out.printf("%d * %d = %d", a,b,c);
+        double f = 51321354768413515346541.3155468651654616515;
+        System.out.printf("%,.3f", f);
+        BufferedReader bufferedReader = new BufferedReader(System.in);
+
+    }
+}
---
+++ SortMax.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Created by leoyats on 03.07.17.
+ */
+public class SortMax {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] sort = new int[3];
+        for (int i = 0; i < sort.length; i++) {
+            sort[i] = Integer.parseInt(reader.readLine());
+        }
+        Arrays.sort(sort);
+        System.out.print(sort[2] + " " + sort[1] + " " + sort[0]);
+
+    }
+    }
+}
